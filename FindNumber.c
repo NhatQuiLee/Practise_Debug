e673593f5002edccba6346a26255a11551b666c6
Nhat Qui Le <nhatquile231001@gmail.com>
2025-04-23 09:49:28 +0700

    Initial commit

---
+++ FindNumber.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+void EnterArray(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("Array[%i] = ", i);
+        scanf("%d", &array[i]);
+    }
+}
+
+void DisplayArray(int array[], int size)
+{
+    printf("\nArray = [ ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("]");
+}
+
+int FindPositionOfNumberInArray(int array[], int size, int number)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] == number)
+        {
+            return i;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int size;
+    int array[20];
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+    EnterArray(array, size);
+    DisplayArray(array, size);
+
+    int number;
+    printf("\nEnter number: ");
+    scanf("%d", &number);
+
+    if (FindPositionOfNumberInArray(array, size, number) == -1)
+    {
+        printf("Number %d is not found in the array", number);
+    }
+    else 
+    {
+        printf("Number %d is found at position %d", number, FindPositionOfNumberInArray(array, size, number));
+    }
+
+    return 0;
+}
